fd03bca58a1a1c40444a54575f4eea532ffd4beb
William Lee <williamlee021@gmail.com>
2020-04-11 22:48:22 -0400

    Updates

---
+++ precache-manifest.9fb566a461cae694aa3a6cef57d616ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3aad8aa78cfb6c024f12701a525451",
+    "url": "/randomapp/index.html"
+  },
+  {
+    "revision": "64ed697ada5ae200214d",
+    "url": "/randomapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f84100ee895d675ce83a",
+    "url": "/randomapp/static/js/2.465ede24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randomapp/static/js/2.465ede24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ed697ada5ae200214d",
+    "url": "/randomapp/static/js/main.50970353.chunk.js"
+  },
+  {
+    "revision": "bdfad21356e7b5d8a821",
+    "url": "/randomapp/static/js/runtime-main.7d9c2699.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/randomapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
